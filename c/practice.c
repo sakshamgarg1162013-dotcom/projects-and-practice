ab5800bbc1ac235f6346966a7f1046f4e4e441b6
Saksham Garg <sakshamgarg116.2013@gmail.com>
2026-03-24 18:19:00 +0530

    daily coding practice

--- c/practice.c
+++ c/practice.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main(){
-    int a[6] = {1,2,3};
-    int b[3] = {2,5,6};
-    bool flag = false;
-    for( int i =0; i<3; i++){
-        int x;
-        for(int j =0 ; j<3+i;j++){
-            if(b[i]==a[j]){
-                flag = true;
-                i++;
-            }
-            else{
-                flag = false;
-            }
-            x=j;
+    int n,m,unit;
+    scanf("%d",&n);
+    m=0;
+    unit=1;
+    while (n!=0)
+    {
+        int ld=n%10;
+        if (ld==0)
+        {
+            ld=1;
         }
-        if(flag==false){
-            a[x+1]=b[i];
-        }
+        ld = ld*unit;
+        n=n/10;
+        m=m+ld;
+        unit=unit*10;
+        
     }
-    for( int i =0; i<6; i++){
-        printf("%d\n",a[i]);
-    }
+    printf("%d",m);
+    return 0;
+
 }
